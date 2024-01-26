4fec7d8f6db0268fc9cfda57e0abfcedb720e626
Jacob Hummer <jcbhmr@outlook.com>
2024-01-26 19:07:35 +0000

    try this

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import * as tc from "@actions/tool-cache"
 import * as github from "@actions/github"
 import { createUnauthenticatedAuth } from "@octokit/auth-unauthenticated"
 import * as semver from "semver"
-import { join } from "node:path"
+import { delimiter, join } from "node:path"
 import { mkdir, readFile, rm, writeFile } from "node:fs/promises"
 
 // Make sure it's still 'dist/index.js' when you change this!
@@ -32,38 +32,30 @@ if (!version) throw new DOMException(`${versionRaw} resolved to ${version}`);
 let found = tc.find("fontist", version);
 core.setOutput("cache-hit", !!found)
 if (!found) {
-  core.info(`Downloading setup-ruby@${setupRubyTag}`)
-  let setupRubyPath = await tc.downloadTool(`https://github.com/ruby/setup-ruby/archive/refs/tags/${setupRubyTag}.zip`)
-  setupRubyPath = await tc.extractZip(setupRubyPath)
-  setupRubyPath = join(setupRubyPath, `setup-ruby-${setupRubyTag.slice(1)}`)
-
-  const tempDir =join(process.env.RUNNER_TEMP!, Math.random().toString())
-  await mkdir(tempDir);
+  const githubPath = await readFile(join(process.env.RUNNER_TEMP!, "GITHUB_PATH"), "utf8")
+  const githubPathItems = githubPath.trimEnd().split(/\r?\n/)
+  const extraPath = githubPathItems.join(delimiter)
 
   let cacheDir = join(process.env.RUNNER_TEMP!, Math.random().toString())
   await mkdir(cacheDir);
   cacheDir = await tc.cacheDir(cacheDir, "fontist", version)
   try {
-    const githubPath = join(tempDir, "GITHUB_PATH")
-    await writeFile(githubPath, "")
-    const githubEnv = join(tempDir, "GITHUB_ENV")
-    await writeFile(githubEnv, "")
     const { all } = await $({
+      all: true,
       env: {
-        RUNNER_TOOL_CACHE: join(cacheDir, "setup-ruby-tool-cache"),
-        GITHUB_PATH: githubPath,
-        GITHUB_ENV: githubEnv,
-        "INPUT_RUBY-VERSION": "ruby",
-        "INPUT_BUNDLER-CACHE": "false",
-      },
-      all: true
-    })`${process.execPath} ${setupRubyPath}/dist/index.js`
+        PATH: `${process.env.PATH}${delimiter}${extraPath}`,
+      }
+    })`gem install fontist --version ${version} --install-dir ${join(cacheDir, "install-dir")} --bindir ${join(cacheDir, "bindir")}`
     core.info(all!)
-    core.info(`GITHUB_PATH=${await readFile(githubPath, "utf8")}`)
-    core.info(`GITHUB_ENV=${await readFile(githubEnv, "utf8")}`)
   } catch (error) {
     core.error(`Failure inside setup block. Removing tool cache folder.`)
     await rm(cacheDir, { recursive: true, force: true })
     throw error
   }
+
+  found = cacheDir
 }
+
+core.addPath(found);
+core.setOutput("fontist-version", version);
+core.info(`✅ Fontist v${version} installed!`);
