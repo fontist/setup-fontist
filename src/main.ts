6d5d012b423ef2eaae6ae51e218421b7df76def8
Jacob Hummer <jcbhmr@outlook.com>
2024-01-27 01:23:22 +0000

    try this

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,8 @@ import { createUnauthenticatedAuth } from "@octokit/auth-unauthenticated";
 import * as semver from "semver";
 import { delimiter, join } from "node:path";
 import { chmod, cp, mkdir, readFile, rm, writeFile } from "node:fs/promises";
-import * as cache from "@actions/cache"
-import * as glob from "@actions/glob"
+import * as cache from "@actions/cache";
+import * as glob from "@actions/glob";
 
 const token = core.getInput("fontist-token");
 const octokit = token
@@ -28,41 +28,54 @@ const version = semver.maxSatisfying(
   versions,
   versionRaw === "latest" ? "*" : versionRaw,
 )!;
-core.debug(`Resolved version: v${version}`);
+core.info(`Resolved version: v${version}`);
 if (!version) throw new DOMException(`${versionRaw} resolved to ${version}`);
 
 let found = tc.find("fontist", version);
-let cacheHit = !!found
+let cacheHit = !!found;
 if (!found) {
-  let cacheDir = join(process.env.RUNNER_TEMP!, Math.random().toString());
-  await mkdir(cacheDir);
-  cacheDir = await tc.cacheDir(cacheDir, "fontist", version);
-  try {
-    await $({
-      stdio: "inherit",
-    })`gem install fontist --version ${version} --install-dir ${join(cacheDir, "install-dir")} --bindir ${join(cacheDir, "bindir")}`;
+  const cacheDir = join(process.env.HOME!, ".fontist-tool-cache");
+  const primaryKey = `fontist-${version}-tool-cache`;
+  const hitKey = core.getBooleanInput("cache") && await cache.restoreCache([cacheDir], primaryKey);
+  cacheHit ||= !!hitKey;
+  if (hitKey) {
+    found = cacheDir;
+    found = await tc.cacheDir(cacheDir, "fontist", version);
+  } else {
+    let cacheDir = join(process.env.RUNNER_TEMP!, Math.random().toString());
+    await mkdir(cacheDir);
+    cacheDir = await tc.cacheDir(cacheDir, "fontist", version);
+    try {
+      await $({
+        stdio: "inherit",
+      })`gem install fontist --version ${version} --install-dir ${join(cacheDir, "install-dir")} --bindir ${join(cacheDir, "bindir")}`;
 
-    await mkdir(join(cacheDir, "bin"));
-    const bash = `\
+      await mkdir(join(cacheDir, "bin"));
+
+      const bash = `\
 #!/bin/bash
 export GEM_PATH=${join(cacheDir, "install-dir")}
 export GEM_HOME=${join(cacheDir, "install-dir")}
-exec ${join(cacheDir, "bindir", "fontist")} "$@"`
-    const cmd = `\
-@echo off
-set GEM_PATH=${join(cacheDir, "install-dir")}
-set GEM_HOME=${join(cacheDir, "install-dir")}
-${join(cacheDir, "bindir", "fontist")} %*`
-    await writeFile(join(cacheDir, "bin", "fontist"), bash)
-    await writeFile(join(cacheDir, "bin", "fontist.cmd"), cmd)
-    await chmod(join(cacheDir, "bin", "fontist"), 0o755)
-    await chmod(join(cacheDir, "bin", "fontist.cmd"), 0o755)
-  } catch (error) {
-    core.error(`Failure inside setup block. Removing tool cache folder.`);
-    await rm(cacheDir, { recursive: true, force: true });
-    throw error;
+exec ${join(cacheDir, "bindir", "fontist")} "$@"`;
+      await writeFile(join(cacheDir, "bin", "fontist"), bash);
+      await chmod(join(cacheDir, "bin", "fontist"), 0o755);
+
+      const cmd = `\
+@echo off\r
+set GEM_PATH=${join(cacheDir, "install-dir")}\r
+set GEM_HOME=${join(cacheDir, "install-dir")}\r
+${join(cacheDir, "bindir", "fontist")} %*`;
+      await writeFile(join(cacheDir, "bin", "fontist.cmd"), cmd);
+    } catch (error) {
+      core.error(`Failure inside setup block. Removing tool cache folder.`);
+      await rm(cacheDir, { recursive: true, force: true });
+      throw error;
+    }
+    found = cacheDir;
+    if (core.getBooleanInput("cache")) {
+      await cache.saveCache([found], primaryKey);
+    }
   }
-  found = cacheDir;
 }
 
 core.addPath(join(found, "bin"));
@@ -70,15 +83,15 @@ core.setOutput("fontist-version", version);
 core.info(`✅ Fontist v${version} installed!`);
 
 if (core.getBooleanInput("cache")) {
-  const cacheDir = join(process.env.HOME!, ".fontist")
-  const primaryKey = `fontist-home`
-  core.saveState("cache-primary-key", primaryKey)
-  const hitKey = await cache.restoreCache([cacheDir], primaryKey)
-  core.saveState("cache-hit", hitKey)
-  cacheHit ||= !!hitKey
+  const cacheDir = join(process.env.HOME!, ".fontist");
+  const primaryKey = `fontist-home`;
+  core.saveState("cache-primary-key", primaryKey);
+  const hitKey = await cache.restoreCache([cacheDir], primaryKey);
+  core.saveState("cache-hit", hitKey);
+  cacheHit ||= !!hitKey;
 }
 
-core.setOutput("cache-hit", cacheHit)
+core.setOutput("cache-hit", cacheHit);
 
-core.info(`Running 'fontist update'...`)
+core.info(`Running 'fontist update'...`);
 await $({ stdio: "inherit" })`fontist update`;
